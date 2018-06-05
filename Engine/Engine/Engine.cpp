02d7a6fd3c1091e5d5d15f3b10749e39abfba294
gamezatwork <gamez.at.work@gmail.com>
2018-06-05 14:04:53 +0800

    - First commit!

---
+++ Engine/Engine/Engine.cpp
@@ -0,0 +1,13 @@
+// Engine.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "HI" << std::endl;
+    return 0;
+}
+
